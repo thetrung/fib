7457b010738d8a67c890da372b985998c29c0e40
Jack Dragoon <deulamco@iMac-deulamco.local>
2022-04-20 20:07:11 +0700

    add build target for tree_tail

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ mkdir ./build
 
 rustc fib.rs -o ./build/fib
 rustc fib_tree.rs -o ./build/fib_tree
+rustc fib_tree_tail.rs -o ./build/fib_tree_tail
 rustc fib_stack.rs -o ./build/fib_stack
 rustc fib_stack_tail.rs -o ./build/fib_stack_tail
