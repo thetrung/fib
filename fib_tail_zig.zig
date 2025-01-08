8ed63ab8ffc70e1e0669ed1e102538001a4674c2
Trung Nguyen <deulamco@gmail.com>
2025-01-08 13:51:45 +0700

    re-enable printing results

--- fib_tail_zig.zig
+++ fib_tail_zig.zig
@@ -1,20 +1,20 @@
-fn fib (n: i128, a: i128, b: i128, loop_count: *i128) i128 {
+fn fib(n: i128, a: i128, b: i128, loop_count: *i128) i128 {
     loop_count.* += 1;
-    switch (n){
+    switch (n) {
         0 => return a,
         1 => return b,
-        else => { 
-            return fib (n - 1, b, a + b, loop_count);       
-        }      
-    } 
+        else => {
+            return fib(n - 1, b, a + b, loop_count);
+        },
+    }
 }
 
 const std = @import("std");
 const print = std.debug.print;
 
-pub fn main () !void {
-    var loop_count:i128 = 0;
-    _ = fib(93, 0, 1, &loop_count);
-    // const r =  fib(93, 0, 1, &loop_count);
-    // print("acc = {} / loop: {}", .{r, loop_count});
+pub fn main() !void {
+    var loop_count: i128 = 0;
+    // _ = fib(93, 0, 1, &loop_count);
+    const r = fib(93, 0, 1, &loop_count);
+    print("acc = {} / loop: {}\n", .{ r, loop_count });
 }
