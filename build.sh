302f5511c2900d8426a1721520cb919725d3cdf4
Trung Nguyen <deulamco@gmail.com>
2025-01-08 13:25:54 +0700

    update build.sh to build llvm & zig implementations

--- build.sh
+++ build.sh
@@ -8,3 +8,6 @@ rustc fib_tree_tail.rs -o ./build/fib_tree_tail
 rustc fib_stack.rs -o ./build/fib_stack
 rustc fib_stack_tail.rs -o ./build/fib_stack_tail
 rustc 1byte_format.rs -o ./build/1byte_format
+llc -filetype=obj fib_llvm.ll -o ./build/fib_llvm.o && clang -no-pie ./build/fib_llvm.o -o ./build/fib_llvm && rm ./build/fib_llvm.o
+zig build-exe fib.zig && mv fib ./build/fib_tail_zig && rm fib.o
+
