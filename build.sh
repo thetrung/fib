e64c1a03dc11d08446a4ee9e795d9435efd0cd46
Trung Nguyen <deulamco@gmail.com>
2025-02-08 22:48:03 +0700

    add fib for FASM version

--- build.sh
+++ build.sh
@@ -15,10 +15,11 @@ rustc 1byte_format.rs -o ./build/1byte_format
 rustc fib_tail_rust.rs -o ./build/fib_tail_rust
 llc -filetype=obj fib_tail_llvm.ll -o ./build/fib_tail_llvm.o && clang -no-pie ./build/fib_tail_llvm.o -o ./build/fib_tail_llvm && rm ./build/fib_tail_llvm.o
 zig build-exe fib_tail_zig.zig && mv fib_tail_zig ./build/fib_tail_zig && rm fib_tail_zig.o
+fasm fib_fasm.asm && mv fib_fasm ./build/fib_fasm && chmod +x ./build/fib_fasm
 
 # Benchmark with hyperfine :
 if [ hyperfine ]; then 
-    cd ./build && hyperfine './fib_tail_llvm' './fib_tail_zig' './fib_tail_rust'
+    cd ./build && hyperfine -N --prepare './fib_tail_llvm' './fib_tail_zig' './fib_tail_rust' './fib_fasm'
     echo ''
 
     # Delete ?
--- fib_tail_zig.zig
+++ fib_tail_zig.zig
@@ -1,6 +1,6 @@
 fn fib(n: i128, a: i128, b: i128, loop_count: *i128) i128 {
     loop_count.* += 1;
-    print("\nn = {} a = {} b = {}", .{ n, a, b });
+    // print("\nn = {} a = {} b = {}", .{ n, a, b });
     switch (n) {
         0 => return a,
         1 => return b,
