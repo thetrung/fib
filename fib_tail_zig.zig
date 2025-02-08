2bdbd70adc7d05fe5d6db98d0adeba25e4d98694
Trung Nguyen <deulamco@gmail.com>
2025-02-08 22:29:30 +0700

    print before compute

--- fib_tail_zig.zig
+++ fib_tail_zig.zig
@@ -1,5 +1,6 @@
 fn fib(n: i128, a: i128, b: i128, loop_count: *i128) i128 {
     loop_count.* += 1;
+    print("\nn = {} a = {} b = {}", .{ n, a, b });
     switch (n) {
         0 => return a,
         1 => return b,
@@ -15,6 +16,7 @@ const print = std.debug.print;
 pub fn main() !void {
     var loop_count: i128 = 0;
     // _ = fib(93, 0, 1, &loop_count);
-    const r = fib(94, 0, 1, &loop_count);
-    print("acc = {} / loop: {}\n", .{ r, loop_count });
+    // => 12200160415121876738
+    const r = fib(93, 0, 1, &loop_count);
+    print("\nacc = {} / loop: {}\n", .{ r, loop_count });
 }
