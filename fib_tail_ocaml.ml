d1fe2126d756c6c39e983444785939ada8fd05b0
Jack Dragoon <deulamco@gmail.com>
2025-11-06 21:29:38 +0700

    add memory support.

--- fib_tail_ocaml.ml
+++ fib_tail_ocaml.ml
@@ -1,7 +1,9 @@
 type opcode =
   | HALT
-  | MOV_REG_REG of int * int     (* dst, src *)
-  | LOAD_IMM of int * int        (* dst, imm *)
+  | MOV_MEM_MEM of int * int    (* dst, src *)
+  | EQU_IMM of int * int   (* dst, imm *)
+  | LOAD_MEM_IMM of int * int   (* dst, imm *) 
+  | MALLOC_INT of int    (* int    x [imm] *)
 
   | ADD of int * int       (* dst, src *)
   | SUB of int * int 
@@ -13,8 +15,8 @@ type opcode =
   | LABEL of string              (* imm *)
   | JMP of int                   (* imm *)
   | CMP_IMM_IMM of int * int     (* imm, imm *)
-  | CMP_REG_REG of int * int     (* reg, reg *)
-  | CMP_REG_IMM of int * int     (* reg, imm *)
+  | CMP_MEM_MEM of int * int     (* reg, reg *)
+  | CMP_MEM_IMM of int * int     (* reg, imm *)
   | JMP_EQUAL of int             (* imm *)
   | JMP_LESSER of int            (* imm *)
   | JMP_GREATER of int           (* imm *)
@@ -22,7 +24,7 @@ type opcode =
   | PRINT of int          (* reg *)
 
 type vm = {
-  mutable registers : int array;
+  mutable memory : int array;
   mutable flags : int array;
   mutable ip : int;
   mutable debug : bool;
@@ -30,56 +32,65 @@ type vm = {
 }
 
 let create_vm program = {
-  registers = Array.make 8 0;
+  memory = Array.make 4 0;     (* only 4 registers *)
   flags = Array.make 3 0;
   ip = 0; debug = false;
   program;
 }
 
-let is_equal, is_greater, is_lesser = 0,1,2
-let ax, bx, cx, dx, ex = 0,1,2,3,4
-(* NOTE :
- * Need a Jump Table already : to store Labels position specifically.
- * Need SFR for CMP Flags. 
- *)
+(* Variables  *)
+let a, b, c, d = 0,1,2,3
 
+(* Comparisons  *)
+let is_equal, is_greater, is_lesser = 0,1,2
+
+(* 
+ * NOTE :
+ * Need a Jump Table : to store Labels position specifically. 
+ *)
 let run vm =
   let len = Array.length vm.program in
   if vm.debug then Printf.printf "\nBEGIN (%d instructions)\n" len;
   while vm.ip < len do
     if vm.debug then 
       Printf.printf "OPCODE: %d\n[ %d | %d | %d | %d ]\n" vm.ip
-      vm.registers.(ax) vm.registers.(bx) vm.registers.(cx) vm.registers.(dx);
+      vm.memory.(a) vm.memory.(b) vm.memory.(c) vm.memory.(d);
     match vm.program.(vm.ip) with
     | HALT -> vm.ip <- len + 1;
-    | MOV_REG_REG (dst, src) ->
-        vm.registers.(dst) <- vm.registers.(src);
+    | MOV_MEM_MEM (dst, src) ->
+        vm.memory.(dst) <- vm.memory.(src);
         vm.ip <- vm.ip + 1;
-    | LOAD_IMM (dst, imm) ->
-        vm.registers.(dst) <- imm;
+    | EQU_IMM (dst, imm) ->
+        vm.memory.(dst) <- imm;
+        vm.ip <- vm.ip + 1;
+    | LOAD_MEM_IMM (dst, imm) ->
+        vm.memory.(dst) <- imm;
+        vm.ip <- vm.ip + 1;
+    | MALLOC_INT imm ->
+        vm.memory <- Array.make imm 0;
         vm.ip <- vm.ip + 1;
     | ADD (dst, src) ->
-        vm.registers.(dst) <- vm.registers.(dst) + vm.registers.(src);
+        vm.memory.(dst) <- vm.memory.(dst) + vm.memory.(src);
         vm.ip <- vm.ip + 1;
     | SUB (dst, src) ->
-        vm.registers.(dst) <- vm.registers.(dst) - vm.registers.(src);
+        vm.memory.(dst) <- vm.memory.(dst) - vm.memory.(src);
         vm.ip <- vm.ip + 1;
     | MUL (dst, src) ->
-        vm.registers.(dst) <- vm.registers.(dst) * vm.registers.(src);
+        vm.memory.(dst) <- vm.memory.(dst) * vm.memory.(src);
         vm.ip <- vm.ip + 1;
     | DIV (dst, src) ->
-        if vm.registers.(src) = 0 then failwith "Division by zero"
-        else vm.registers.(dst) <- vm.registers.(dst) / vm.registers.(src);
+        if vm.memory.(src) = 0 then failwith "Division by zero"
+        else vm.memory.(dst) <- vm.memory.(dst) / vm.memory.(src);
         vm.ip <- vm.ip + 1;
     | INC src -> 
-        vm.registers.(src) <- vm.registers.(src) + 1;
+        vm.memory.(src) <- vm.memory.(src) + 1;
         vm.ip <- vm.ip + 1;
     | DEC src -> 
-        vm.registers.(src) <- vm.registers.(src) - 1;
-        if vm.debug then Printf.printf "DEC => %d\n" vm.registers.(src);
+        vm.memory.(src) <- vm.memory.(src) - 1;
+        if vm.debug then Printf.printf "DEC => %d\n" vm.memory.(src);
         vm.ip <- vm.ip + 1;
     | PRINT reg ->
-        Printf.printf (if vm.debug then "PRINT: %d\n" else "%d\n") vm.registers.(reg);
+        Printf.printf (if vm.debug then "PRINT: %d\n" else "%d\n") vm.memory.(reg);
         vm.ip <- vm.ip + 1;
     | LABEL name ->
         if vm.debug then Printf.printf "\nLABEL: %s @ %d\n" name vm.ip;
@@ -98,8 +109,8 @@ let run vm =
         if vm.flags.(is_greater) == 1 
         then vm.ip <- ip
         else vm.ip <- vm.ip + 1;
-    | CMP_REG_REG (src1, src2) -> 
-      let regs = vm.registers in
+    | CMP_MEM_MEM (src1, src2) -> 
+      let regs = vm.memory in
         if regs.(src1) == regs.(src2) then vm.flags.(is_equal) <- 1 else 
         if regs.(src1) > regs.(src2) then vm.flags.(is_greater) <- 1 else 
         if regs.(src1) < regs.(src2) then vm.flags.(is_lesser) <- 1 else 
@@ -111,8 +122,8 @@ let run vm =
         if imm1 <  imm2 then vm.flags.(is_lesser)  <- 1 else 
           failwith "Unsupported CMP instruction.";
         vm.ip <- vm.ip + 1;
-    | CMP_REG_IMM (src, imm) -> 
-      let regs = vm.registers in
+    | CMP_MEM_IMM (src, imm) -> 
+      let regs = vm.memory in
         if regs.(src) == imm then vm.flags.(is_equal)   <- 1 else 
         if regs.(src) >  imm then vm.flags.(is_greater) <- 1 else 
         if regs.(src) <  imm then vm.flags.(is_lesser)  <- 1 else 
@@ -124,20 +135,20 @@ let run vm =
   
 let () = 
   let fib = [|
-    LOAD_IMM (ax, 0);          (* a = 0 *)
-    LOAD_IMM (bx, 1);          (* b = 1 *)
-    LOAD_IMM (cx, 90);         (* c = 90 => 2880067194370816120 *)
+    EQU_IMM (a, 0);          (* a = 0 *)
+    EQU_IMM (b, 1);          (* b = 1 *)
+    EQU_IMM (c, 90);         (* c = 90 => 2880067194370816120 *)
     LABEL("Entry");
-    CMP_REG_IMM (cx, 0);       (* c == 0 ? *)
+    CMP_MEM_IMM (c, 0);       (* c == 0 ? *)
     JMP_EQUAL (13);            (* return a *)
-    CMP_REG_IMM (cx, 1);       (* c == 1 ? *)
+    CMP_MEM_IMM (c, 1);       (* c == 1 ? *)
     JMP_EQUAL (13);            (* return b *)
-    MOV_REG_REG (dx, ax);      (* d = a *)
-    MOV_REG_REG (ax, bx);      (* a = b *)
-    ADD (bx, dx);              (* b = d + b *)
-    DEC (cx);                  (* c = c - 1 *)
+    MOV_MEM_MEM (d, a);      (* d = a *)
+    MOV_MEM_MEM (a, b);      (* a = b *)
+    ADD (b, d);              (* b = d + b *)
+    DEC (c);                  (* c = c - 1 *)
     JMP (3);                   (* -> Entry 3 *)
     LABEL ("Exit");
-    PRINT (bx);                (* print a *)
+    PRINT (b);                (* print a *)
     HALT;                      (* end program *)
   |] in let vm = create_vm fib in run vm;
